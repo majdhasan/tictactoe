76da8999fea20239b4afd6a5ca90320acef428f1
Majd Hasan <majd.hasan@aperto.com>
2020-12-13 00:17:55 +0100

    add game and view conditionaly

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Game() {
     return (
         <div>
-
+            <h1>Game is on </h1>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import socketClient from 'socket.io-client'
 
 import { Navigation, CreateGameForm, JoinGameForm } from '../components'
+import { Game } from './Game';
 
 function Home() {
 
-    const handleSubmit = (name, id) => {
+    const [showGame, setShowGame] = useState(false)
 
+    const handleSubmit = (name, id) => {
         console.log(`name: ${name} and id: ${id}`);
+        setShowGame(true)
     }
 
     return (
         <div>
-            {/* <Navigation></Navigation> */}
             <div className="container">
-                <div className="home-header">
-                    Tic Tac Win
-                </div>
-                <p className="text-muted">Start a new game or join an existing game</p>
-
-                <hr />
-                <CreateGameForm onSubmit={handleSubmit} />
-                <hr />
-                <JoinGameForm onSubmit={handleSubmit} />
+                {!showGame && (
+                    <>
+                        <div className="home-header">
+                            Tic Tac Win
+                        </div>
+                        <p className="text-muted">Start a new game or join an existing game</p>
+                        <hr />
+                        <CreateGameForm onSubmit={handleSubmit} />
+                        <hr />
+                        <JoinGameForm onSubmit={handleSubmit} />
+                    </>
+                )}
+                {showGame && <Game />}
             </div>
         </div>
     )
