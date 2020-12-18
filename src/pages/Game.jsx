93846901a0316a5014f520a2823c38a122a42bd3
Majd Hasan <majd.hasan@aperto.com>
2020-12-18 19:48:37 +0100

    add the game board with some functionalities

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -6,8 +6,8 @@ function Game({ gameId, name }) {
 
     const SERVER = 'http://localhost:5001/'
 
-    const [player, setPlayer] = useState({})
-    const [game, setGame] = useState({})
+    const [player, setPlayer] = useState(null)
+    const [game, setGame] = useState(null)
     const socketRef = useRef();
 
     useEffect(() => {
@@ -23,15 +23,49 @@ function Game({ gameId, name }) {
     useEffect(() => {
         socketRef.current.on('playerCreated', data => {
             console.log("playerCreated", data);
+            setGame(data.game)
+            setPlayer(data.player)
+            console.log(player);
         })
 
         socketRef.current.on('gameUpdated', data => {
             console.log("gameUpdated", data);
+            setGame(data.game.playBoard)
         })
     });
+
+    const handleClick = e => {
+        const { name } = e.target;
+        const newPlayBoard = game.playBoard
+        newPlayBoard[name] = player.symbol
+        setGame({ ...game, playBoard: newPlayBoard })
+        socketRef.current.emit("gameChanged", game)
+        console.log(`the box with the name ${name} has been clicked`);
+    }
     return (
-        <div>
-            <h1>Game is on </h1>
+
+        <div className="home-header">
+            { player && <p>Hello {player.name}</p>}
+            <h1>Its your Turn 👇</h1>
+            <div className="board">
+
+                {game && game.playBoard.map((box, index) => {
+
+                    if (player && game && player.id !== game.playerTurn) {
+                        return (
+                            <div className="box" id={`box-${index}`}>
+                                <button disabled key={index + "box"} name={"box" + index} onClick={handleClick} className="box-button">{box}</button>
+                            </div>
+                        )
+                    } else {
+                        return (
+                            <div className="box" id={`box-${index}`}>
+                                <button key={index + "box"} name={index} onClick={handleClick} className="box-button">{box}</button>
+                            </div>
+                        )
+                    }
+                })}
+            </div>
         </div>
     )
 }
